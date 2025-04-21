19ae5da637aa2160bdb1095e52d1f397e7ce770c
darwinweng2002 <vdarwin860@gmail.com>
2025-04-21 11:36:37 +0700

    up

--- index.php
+++ index.php
@@ -124,7 +124,7 @@
 <body>
     <div class="container">
         <header>
-            <h1>tangina and Property Management Services System</h1>
+            <h1>Supply and Property Management Services System</h1>
         </header>
         <div class="login-box">
             <h2>Administrator Login</h2>
