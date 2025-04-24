3404a6d0f6fa80277204c9d8861834c55a11b797
darwinweng2002 <vdarwin860@gmail.com>
2025-04-24 10:40:03 +0700

    up

--- update_admin.php
+++ update_admin.php
@@ -162,7 +162,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             padding: 25px; 
             border-radius: 10px; 
             box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); 
-            width: 400px; 
+            width: 900px; 
             position: relative;
             padding-bottom: 85px;
         }
