cf81bf882a8f670f18045d79cfd9ebbb17e07f21
darwinweng2002 <vdarwin860@gmail.com>
2025-04-30 16:01:12 +0700

    up

--- view_excel_files.php
+++ view_excel_files.php
@@ -231,7 +231,7 @@ footer img.footer-logo {
                     <span class="input-group-text bg-primary text-white">
                         <i class="bi bi-search"></i>
                     </span>
-                    <input type="text" id="searchInput" class="form-control mb-3" placeholder="Search files..." onkeyup="searchFiles()">
+                    <input type="text" id="searchInput" class="form-control mb-5" placeholder="Search files..." onkeyup="searchFiles()">
                 </div>
     <?php if (!empty($files)): ?>
         <table class="table table-striped file-table">
